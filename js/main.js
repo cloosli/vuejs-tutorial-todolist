bb843dd88c0f01da0c9cfc55ca34da17e20b9e7b
Christian Loosli <loosli.christian@gmail.com>
2016-09-26 21:32:23 +0200

    Todos for list and if-else case

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!'
-  }
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!',
+        todos: [
+          {
+            title: 'Todo 1'
+        }, {
+            title: 'Todo 2'
+        }, {
+            title: 'Todo 3'
+        }],
+        showTodos:true
+    }
 })
