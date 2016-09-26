b85bd9c4f875d43f7797e9b6028db8d8d994d3e2
Christian Loosli <loosli.christian@gmail.com>
2016-09-26 22:21:54 +0200

    Edit Todos Using Components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+var TodoComponent = Vue.extend({
+    data: function() {
+        return {
+            inEditMode: false
+        };
+    },
+    props: ['todo'],
+    template: '<div><span v-on:click="clicked" v-show="!inEditMode">{{ todo.title }}</span><input v-on:keyup.enter="saved" v-show="inEditMode" v-model="todo.title"/></div>',
+    methods: {
+        clicked: function() {
+            this.inEditMode = true;
+        },
+        saved: function() {
+            this.inEditMode = false;
+        }
+    }
+});
+
+Vue.component('todo-component', TodoComponent);
+
 new Vue({
     el: '#app',
     data: {
